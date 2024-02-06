cb316c999a5dee53f000bf20d085276278848484
Thifj <thifj.j@hotmail.com>
2024-02-05 22:20:22 -0300

    refazendo as views

--- resources/views/agoraweb/home.blade.php
+++ resources/views/agoraweb/home.blade.php
@@ -1,1 +1,6 @@
-<h1>Home</h1>
+@extends('agoraweb.layouts.layout')
+@section('title', 'Pagina Principal')
+@section('content')
+    <h1>Bem vindo a Pagina principal (Famosa home)</h1>
+    <p>Descrubra as opcoes em nosso site!</p>
+@endsection
