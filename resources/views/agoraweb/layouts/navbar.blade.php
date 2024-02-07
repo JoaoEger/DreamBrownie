e40e4b0389710e55178e3d547e602bdd516c2d3b
Thifj <thifj.j@hotmail.com>
2024-02-07 11:17:34 -0300

    adicionando cor na navbar

--- resources/views/agoraweb/layouts/navbar.blade.php
+++ resources/views/agoraweb/layouts/navbar.blade.php
@@ -1,14 +1,14 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary navfix">
-    <div class="container-fluid red">
+    <div class="container-fluid navbackground">
       <a class="navbar-brand" href="home"><img src="logoSemFundo.png" alt="Nao foi Possivel carregar a imagem" height="85" width="65"></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-          <a class="nav-link" aria-current="page" href="home">Home</a>
-          <a class="nav-link" href="padarias">Padarias</a>
-          <a class="nav-link" href="account">Conta</a>
+          <a class="nav-link colornav" aria-current="page" href="home">Home</a>
+          <a class="nav-link colornav" href="padarias">Padarias</a>
+          <a class="nav-link colornav" href="account">Conta</a>
         </div>
       </div>
     </div>
