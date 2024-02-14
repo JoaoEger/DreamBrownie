d6771df4bab197cd0a2650fc50f8d14d39d4e23b
Thifj <thifj.j@hotmail.com>
2024-02-14 11:11:58 -0300

    Deixando todas as views conectadas com o layout e modif padaria, cadastro

--- resources/views/agoraweb/produtos.blade.php
+++ resources/views/agoraweb/produtos.blade.php
@@ -1,6 +1,6 @@
-@extends('agoraweb.layouts.layoutSidebar.formato')
-@section('titulo', 'Pagina Principal')
-@section('conteudo')
+@extends('agoraweb.layouts.layout')
+@section('title', 'Produtos')
+@section('content')
     <h1>Bem vindo a Pagina produtos (Famosa products)</h1>
     <p>Descrubra as opcoes em nosso site!</p>
 @endsection
