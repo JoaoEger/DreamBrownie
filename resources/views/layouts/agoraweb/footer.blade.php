0fb862fdcd3741660731480339a15551835f3d69
Thifj <thifj.j@hotmail.com>
2024-02-14 17:36:33 -0300

    mudando corzinha do layout

--- resources/views/layouts/agoraweb/footer.blade.php
+++ resources/views/layouts/agoraweb/footer.blade.php
@@ -1,13 +1,13 @@
-<footer class="footer bg-success py-3">
+<footer class="footer bg-cor py-3">
   <div class="container">
       <div class="row">
           <div class="col-md-6">
-              <p>&copy; 2024 AgoraWeb Developing. Todos os direitos reservados.</p>
+              <p class="txt-cor">&copy; 2024 AgoraWeb Developing. Todos os direitos reservados.</p>
           </div>
           <div class="col-md-6 text-md-right">
               <ul class="list-inline">
-                  <li class="list-inline-item"><a href="/termos">Termos de Serviço</a></li>
-                  <li class="list-inline-item"><a href="/politica-de-privacidade">Política de Privacidade</a></li>
+                  <li class="list-inline-item"><a class="txt-cor" href="/termos">Termos de Serviço</a></li>
+                  <li class="list-inline-item"><a class="txt-cor" href="/politica-de-privacidade">Política de Privacidade</a></li>
               </ul>
           </div>
       </div>
--- resources/views/layouts/agoraweb/navbar.blade.php
+++ resources/views/layouts/agoraweb/navbar.blade.php
@@ -1,5 +1,5 @@
-<nav class="navbar navbar-expand-lg bg-success">
-    <div class="container-fluid">
+<nav class="navbar navbar-expand-lg bg-cor">
+    <div class="container-fluid">g
         <a class="navbar-brand" href="/home"><a class="navbar-brand" href="home"><img src="logoSemFundo.png"
                     alt="Nao foi Possivel carregar a imagem" height="75" width="65"></a></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
@@ -9,21 +9,21 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/home">Home</a>
+                    <a class="nav-link active txt-cor" aria-current="page" href="/home">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/padarias">Padaria</a>
+                    <a class="nav-link txt-cor txt-cor" href="/padarias">Padaria</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/produtos">Produtos</a>
+                    <a class="nav-link txt-cor" href="/produtos">Produtos</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/cadastro">Cadastro</a>
+                <li class="nav-item ">
+                    <a class="nav-link txt-cor " href="/cadastro">Cadastro</a>
                 </li>
             </ul>
             <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Buscar produtos" aria-label="Search">
-                <button class="btn btn-outline-warning" type="submit">Buscar</button>
+                <input class="form-control me-2 border-brown" type="search" placeholder="Buscar produtos" aria-label="Search">
+                <button class="btn border-brown txt-cor" type="submit">Buscar</button>
             </form>
         </div>
     </div>
