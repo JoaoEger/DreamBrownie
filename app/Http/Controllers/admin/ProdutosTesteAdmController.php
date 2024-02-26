78b30b33d09af0f97c1ef90c59a6fe0a63f4b585
Thifj <thifj.j@hotmail.com>
2024-02-26 17:16:49 -0300

    arrumando erros

--- app/Http/Controllers/admin/ProdutosTesteAdmController.php
+++ app/Http/Controllers/admin/ProdutosTesteAdmController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Models\Padarias;
 use App\Models\Produtos;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+use function PHPUnit\Framework\fileExists;
 
 class ProdutosTesteAdmController extends Controller
 {
@@ -14,7 +17,7 @@ class ProdutosTesteAdmController extends Controller
      */
     public function index()
     {
-        return view("admin/produtos/produtosteste", [
+        return view("admin/produtos/produtos", [
             "produtos" => Produtos::OrderBy("id", "DESC")->paginate(5),
         ]);
     }
@@ -46,16 +49,14 @@ class ProdutosTesteAdmController extends Controller
         $produtos->date = date("Y-m-d h:i:s");
 
         //upload da imagem
-        if($request->hasfile("image")&&$request->file("image")->isValid()){
-            $nome = strtotime("now").".".$request->image->extension();
+        if ($request->hasfile("image") && $request->file("image")->isValid()) {
+            $nome = strtotime("now") . "." . $request->image->extension();
             $request->image->move(public_path("upload"), $nome);
             $produtos->image = $nome;
         }
-        
-        $produtos->save();
-        return redirect("/admin/produtosteste")->
-        with("success","Registro inserido com sucesso.");
 
+        $produtos->save();
+        return redirect("/admin/produtos")->with("success", "Registro inserido com sucesso.");
     }
 
     /**
@@ -71,7 +72,10 @@ class ProdutosTesteAdmController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        return view("admin/produtos/edit", [
+            "padaria" => Padarias::all(),
+            "produtos" => Produtos::findOrFail($id)
+        ]);
     }
 
     /**
@@ -79,7 +83,31 @@ class ProdutosTesteAdmController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $produtos = Produtos::find($id);
+        $produtos->nome = $request->nome;
+        $produtos->preview = $request->preview;
+        $produtos->quantidade = $request->quantidade;
+        $produtos->preview = $request->preview;
+        $produtos->valor = $request->valor;
+        $produtos->estoque = $request->estoque;
+        $produtos->from_padarias = $request->from_padarias;
+
+        $imgAtual = $produtos->image;
+        if ($request->hasFile("image") && $request->file("image")->isValid()) {
+            //deletar imagem antiga se tiver
+            if (file_exists(public_path("upload/") . $imgAtual)) {
+                unlink(public_path("upload/") . $imgAtual);
+            }
+
+            //upload
+            $nome = strtotime("now") . "." . $request->image->extension();
+            $request->image->move(public_path("upload"), $nome);
+            $produtos->image = $nome;
+        }
+
+        $produtos->update();
+        return redirect("/admin/produtos")
+            ->with("success", "Produto editado com sucesso");
     }
 
     /**
@@ -87,7 +115,16 @@ class ProdutosTesteAdmController extends Controller
      */
     public function destroy(string $id)
     {
-        Produtos::findOrFail($id)->delete();
-        return redirect("/admin/produtosteste")->with("success","Registro deletado com sucesso.");
+        $produtos = Produtos::find($id);
+
+        //verificar se existe uma imagem para deletar
+        if(file_Exists(public_path("upload/").$produtos->image)){
+            //deleta o arquivo
+            unlink(public_path("upload/").$produtos->image);
+        }
+
+        $produtos->delete();
+        return redirect("/admin/produtos")
+            ->with("success", "Registro deletado com sucesso.");
     }
 }
--- resources/views/admin/produtos/edit.blade.php
+++ resources/views/admin/produtos/edit.blade.php
@@ -3,42 +3,50 @@
 @section('content')
     <div class="row">
         <div class="col-12">
-            <h1>Cadastrar um novo produto</h1>
+            <h1>Editar um produto</h1>
         </div>
     </div>
-    <form action="{{ route('produtos.store') }}" method="POST" enctype="multipart/form-data">
+    <form action="{{ route('produtos.update', $produtos->id) }}" method="POST" enctype="multipart/form-data">
         @csrf
+        @method("PUT")
         <div class="row">
             <div class="col col-sm 12 col-md-4 mb-3">
                 <label for="nome">Nome</label>
-                <input type="text" id="nome" name="nome" class="form-control">
+                <input type="text" id="nome" name="nome" value="{{$produtos->nome}}" class="form-control">
             </div>
             <div class="col col-sm 12 col-md-8 mb-3">
                 <label for="preview">Descricao</label>
-                <input type="text" id="preview" name="preview" class="form-control">
+                <input type="text" id="preview" name="preview" value="{{$produtos->preview}}" class="form-control">
             </div>
             <div class="col col-sm 12 col-md-2 mb-3">
                 <label for="quantidade">Quantidade (un)</label>
-                <input type="text" id="quantidade" name="quantidade" class="form-control">
+                <input type="text" id="quantidade" name="quantidade" value="{{$produtos->quantidade}}" class="form-control">
             </div>
+
             <div class="col col-sm 12 col-md-4 mb-3">
                 <label for="image">imagem</label>
-                <input type="file" id="image" name="image" class="form-control">
+                <div class="input-group">
+                  <label for="image" class="input-group-text">
+                    <img src="/upload/{{$produtos->image}}" width="60px">
+                  </label>
+                  <input type="file" id="image" name="image" class="form-control">
+                </div>
             </div>
+
             <div class="col col-sm 12 col-md-2 mb-3">
                 <label for="valor">Valor em R$</label>
-                <input type="text" id="valor" name="valor" class="form-control">
+                <input type="text" id="valor" name="valor" value="{{$produtos->valor}}" class="form-control">
             </div>
             <div class="col col-sm 12 col-md-2 mb-3">
                 <label for="estoque">Estoque</label>
-                <input type="text" id="estoque" name="estoque" class="form-control">
+                <input type="text" id="estoque" name="estoque" value="{{$produtos->estoque}}" class="form-control">
             </div>
             <div class="col col-sm 12 col-md-4 mb-3">
                 <label for="from_padarias">Padaria</label>
                 <select name="from_padarias" id="from_padarias" class="form-select">
                     <option value="">Selecione uma padaria</option>
                     @foreach ($padaria as $pad)
-                        <option value="{{ $pad->id }}">{{ $pad->nome }}</option>
+                        <option value="{{ $pad->id }}" @if($pad->id == $produtos->from_padarias) selected @endif>{{ $pad->nome }}</option>
                     @endforeach
                 </select>
             </div>
